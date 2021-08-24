2c4e31c14ce9f56fa1e65a052ccdd48d90c63895
manishmydurr <89435086+manishmydurr@users.noreply.github.com>
2021-08-24 23:35:35 +0530

    Add files via upload

---
+++ day6/index6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "hi everyone";
+    
+    $a=1;
+    echo $a;
+    ?>
+    <form action="loginprocess.php" method="post">
+        <label for="">Name:</label>
+        <input type="text" name="name" id="">
+        <input type="submit" value="subnit">
+</form>
+</body>
+</html>
